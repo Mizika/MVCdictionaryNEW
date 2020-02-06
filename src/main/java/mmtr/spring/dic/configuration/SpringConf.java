1db66e3c8c6b8b79d34c044038287c4842706451
Замэ Максим <mzame@mmtr.ru>
2020-02-06 09:45:36 +0300

    Add generation data Hibernate.

--- src/main/java/mmtr/spring/dic/configuration/SpringConf.java
+++ src/main/java/mmtr/spring/dic/configuration/SpringConf.java
@@ -9,13 +9,4 @@ import org.springframework.context.annotation.PropertySource;
 @ComponentScan("mmtr.spring.dic")
 @PropertySource("classpath:dic.properties")
 public class SpringConf {
-//    @Bean
-//    public DataSource datasource() {
-//        return DataSourceBuilder.create()
-//                .driverClassName("org.postgresql.Driver")
-//                .url("jdbc:postgresql://localhost:5432/postgres")
-//                .username("postgres")
-//                .password("12345")
-//                .build();
-//    }
 }
